3db51c5425902c29ca301e64a0646e776e79c38a
Silpa Tom <silpatom@gmail.com>
2024-07-06 21:48:29 +0530

    detailed view page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react"
+import DetailedView from "./pages/DetailedView"
+import { Route, Routes } from "react-router-dom"
 
 
 function App() {
@@ -5,6 +8,10 @@ function App() {
   return (
     <>
    <h1>Welcome</h1>
+   <Routes>
+   <Route path='/detailedview' element={<DetailedView/>}/>
+   </Routes>
+    
     </>
   )
 }
---
+++ src/pages/DetailedView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DetailedView() {
+  return (
+    <div>
+      <h1 style={{textAlign:'center', color:'blueviolet'}}> Detailed View</h1>
+    </div>
+  )
+}
+
+export default DetailedView
